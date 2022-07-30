fafdc7e223797dbaeb8910fa5c385346ad5995ee
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-07-29 18:56:23 -0700

    Completed assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+
+        return response.body
+    end
+
+    def parse_json
+        return JSON.parse(self.get_response_body)
+    end
+end
+
+
+get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.parse_json
